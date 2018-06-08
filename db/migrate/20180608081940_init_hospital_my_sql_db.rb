274039e4309534f7247203df64362596393617e9
zengwei <zengwei@tenmind.com>
2018-06-08 20:56:44 +0800

    up

---
+++ db/migrate/20180608081940_init_hospital_my_sql_db.rb
@@ -0,0 +1,22 @@
+class InitHospitalMySqlDb < ActiveRecord::Migration[5.1]
+  def change
+    create_table :common_addresses do |t|
+      t.string :use
+      t.string :type
+      t.string :province
+      t.string :city
+      t.string :country
+      t.string :township
+      t.string :street
+      t.string :house_number
+      t.string :post_code
+      t.string :detail
+      t.string :period_start
+      t.string :period_end
+      t.string :status
+      t.integer :master_id
+
+      t.timestamps
+    end
+  end
+end
