a0d452bd722dabc13781d51c554990e4e46feabd
qinxiao <qinxiao@tenmind.com>
2018-06-11 19:20:58 +0800

    update

--- app/models/orders/order.rb
+++ app/models/orders/order.rb
@@ -1,7 +1,9 @@
 class Orders::Order < ApplicationRecord
+	has_many :details, class_name: '::Orders::OrderDetail', foreign_key: 'order_id'
+	belongs_to :user, class_name: '::User', foreign_key: 'user_id'
 
 # CREATE TABLE Order(
-#  order_id INT NOT NULL AUTO_INCREMENT,
+#  id INT NOT NULL AUTO_INCREMENT,
 #  created_at DATETIME NOT NULL '创建时间',
 #  updated_at DATETIME NOT NULL '更新时间',
 #  payment_at DATETIME NOT NULL '支付时间',
@@ -15,20 +17,37 @@ class Orders::Order < ApplicationRecord
 #  user_id VARCHAR(32) NOT NULL '用户id',
 #  shipping_name varchar(20)  NULL '物流名称',
 #  shipping_code varchar(20)  NULL '物流单号',
-#  payment_type float NOT NULL '支付类型，1.在线支付，2.货到付款',
-#  status VARCHAR(4) NOT NULL '未付款，已付款，未发货，已发货，交易成功，交易关闭',
+#  payment_type float NOT NULL '支付类型,1.在线支付,2.货到付款',
+#  status VARCHAR(4) NOT NULL '未付款,已付款,未发货,已发货,交易成功,交易关闭',
 #  PRIMARY KEY ( id )
 #  )
+
+# rails generate model Orders::Order payment_at:time end_time:time close_time:time target_org_ii:string target_org_name:string source_org_ii:string source_org_name:string order_code:string user_id:string shipping_name:string shipping_code:string payment_type:float status:string
+
 	class << self
-		#attrs = { target_org_ii:'目标药房的名称和机构', target_org_name:'目标药房的名称和机构', source_org_ii:'来源的医院名称和ii', source_org_name:'来源的医院名称和ii', order_code:'订单号', quantity:'数量',details:[name:'名称',item_id:'商品id',unit:'单位',quantity:'数量'，unit_price:'单价',specifications:'规格', dosage:'剂型']} 
+		#attrs = { target_org_ii:'目标药房的名称和机构', target_org_name:'目标药房的名称和机构', source_org_ii:'来源的医院名称和ii', source_org_name:'来源的医院名称和ii', order_code:'订单号', user_id:'用户id',details:[name:'名称',item_id:'商品id',unit:'2',quantity:'1',price:'单价',specifications:'规格', dosage:'剂型']} 
 		#订单生成创建
 		def create_order attrs = {}
 			attrs = attrs.deep_symbolize_keys
-			order = self.create()
-
+			order_code = get_order_code
+			order = self.create(
+				 target_org_ii: attrs[:target_org_ii],
+				 target_org_name: attrs[:target_org_name],
+				 source_org_ii: attrs[:source_org_ii],
+				 source_org_name: attrs[:source_org_name],
+				 order_code: order_code,
+				 user_id: attrs[:user_id],
+				 status:'N'
+		 		)
+			attrs[:details].each do |detail|
+				net_amt = (detail[:quantity].to_f * detail[:price].to_f).round(2)
+				Orders::OrderDetail.create(detail.merge({order_id:order.id,net_amt:net_amt}))
+			end
+			order
 		end	
 
 		private
+		##获取订单号，私有调用
 		def get_order_code
 			t = Time.now
 			y = t.year.to_s[2,2]
--- app/models/settles/settle.rb
+++ app/models/settles/settle.rb
@@ -1,2 +1,20 @@
 class Settles::Settle < ApplicationRecord
+
+# rails generate model Settles::Settle net_amt:float pay_method:string pay_type:string out_trade_no:string body:string detail:string device_info:string auth_code:string status:string
+
+# CREATE TABLE Settle(
+#  id INT NOT NULL AUTO_INCREMENT,
+#  created_at DATETIME NOT NULL,
+#  updated_at DATETIME NOT NULL,
+#  net_amt DOUBLE NOT NULL,
+#  pay_method VARCHAR(8) NOT NULL,
+#  pay_type VARCHAR(8) NULL,
+#  out_trade_no VARCHAR(32) NULL,
+#  body VARCHAR(128) NULL,
+#  detail VARCHAR(6000) NULL,
+#  device_info VARCHAR(32) NULL,
+#  auth_code VARCHAR(128) NULL,
+#  status VARCHAR(4) NOT NULL,
+#  PRIMARY KEY ( id )
+#  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 end
