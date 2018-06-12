4e8dd05f4e5d05b3784f0565472372310045cbd3
wml <wml@tm.com>
2018-06-12 18:09:31 +0800

    update

--- app/models/ims/order.rb
+++ app/models/ims/order.rb
@@ -11,8 +11,38 @@ class Ims::Order < ApplicationRecord
   end
 
   # 订单退药
-  def return_order
-  	self.update_attributes(status:"C")
+  def return_order current_user
+	  begin
+	  	returned_at = Ims::Setting.where(:org_ii=>current_user.try(:org_ii)).try(:returned_at)
+	  	return {flag:false,info:"该单已经退过药了。"} if self.this_returned
+	  	return {flag:false,info:"退药失败。已超过退药期(#{returned_at})天"} if self.operat_at.to_time<(Time.new-returned_at.to_i)
+	  	result = return_order_create current_user
+		  self.update_attributes(status:"C") if result[:flag]
+		  retrun result
+	  rescue Exception => e
+	  	print e.message rescue "  e.messag----"
+      print "laaaaaaaaaaaaaaaaaaaa 订单发药 出错: " + e.backtrace.join("\n")
+      result = {flag:false,:info=>"系统出错。"}
+	  end
+  end
+
+  # 退药的处方保存
+  def return_order_create current_user
+  	begin
+	  	order_id = BSON::ObjectId.new.to_s
+	    lines,ids = [],[]
+	    order = self.attributes.deep_symbolize_keys
+	    new_code = "T#{self.order_code}"
+	    order=order.delete_if {|k,v| (k==:_id || k==:created_at || k==:updated_at || k==:line_ids)}
+	    update_data = {operater_id:current_user.try(:id).to_s,operater_name:current_user.try(:name).to_s,operat_at:Time.new,ori_code:self.order_code,ori_id:self.id.to_s,:status=>"C",code:new_code,id:order_id}   #需要更新的字段
+	    order.merge!(update_data)
+	    h = self.class.create(order)
+	    l&&h ? {flag:true,info:"退药成功！"} : {flag:false,info:"退药失败。"}
+  	rescue Exception => e
+  		print e.message rescue "  e.messag----"
+      print "laaaaaaaaaaaaaaaaaaaa 订单发药 出错: " + e.backtrace.join("\n")
+      result = {flag:false,:info=>"系统出错。"}
+  	end
   end
 
 
@@ -24,7 +54,7 @@ class Ims::Order < ApplicationRecord
         status = args[:status]
         search = args[:search]
         org_ii = args[:org_ii]
-        query = {target_org_ii:org_ii,search_name:/#{search}/i}
+        query = {org_ii:org_ii,search_name:/#{search}/i}
         query[:status] = status if status.blank?
     		self.where(query)
       rescue Exception => e
@@ -49,6 +79,11 @@ class Ims::Order < ApplicationRecord
   	def receive_order args={}
   		begin
   			args = args.deep_symbolize_key
+  			order = args[:order_code]
+  			
+
+
+
   			patient_order_id = args.delete :id
   			self_sym = self.new.attribute_names.map(&:to_sym)
   			args.keep_if {|k,v| self_sym.include?(k)}
