f8e6de2c3715b6367a266d6188eeb1d5dfc08f10
qinxiao <qinxiao@tenmind.com>
2018-06-19 22:47:38 +0800

    update

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -74,7 +74,7 @@ class PayController < ApplicationController
       return write_ali_log("------订单未找到---#{params['out_trade_no']}") unless ali
       return write_ali_log("------订单未支付----#{params['trade_status']}") unless params['trade_status'].eql?('TRADE_SUCCESS')
       if params['total_amount'].to_f == ali.total_fee.to_f
-        Orders::Order.find(ali.out_trade_no).order_settle("Alipay")
+        Orders::Order.find(ali.out_trade_no.split('_')[0]).order_settle("Alipay")
         return write_ali_log("-----订单已支付----#{params['out_trade_no']}") if ali.update_attributes({status: 'success', status_desc: '订单已支付'})
         write_ali_log("------订单已支付但数据更新失败----原因:#{ali.errors.full_messages.join(',')}")
       else
