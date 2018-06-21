ef70262c58811ce300b86854067b7fa88bb8590a
zengwei <zengwei@tenmind.com>
2018-06-21 10:50:46 +0800

    up

--- app/controllers/dict/diseases_controller.rb
+++ app/controllers/dict/diseases_controller.rb
@@ -1,86 +1,37 @@
 class ::Dict::DiseasesController < ApplicationController
-	# before_action :set_disease, only: [:show, :edit, :update, :destroy]
+	rescue_from RuntimeError do |exception|
+		respond_to do |html|
+			html.html {render xml:{flag:false,reason:exception.class.to_s,info:exception.message}}
+			html.json {render json:{flag:false,reason:exception.class.to_s,info:exception.message}}
+		end
+	end
+
+
+	# 查询病种字典返回ICD10诊断列表	
 	# GET
 	# /dict/diseases
 	def index
-		p "::Dict::DiseasesController index"
+		p "::Dict::DiseasesController index", params
 		search = params[:search].to_s
-		@diseases = ::Dict::Disease.where("ICD10 LIKE ?  OR name LIKE ? OR py LIKE ?", "%#{search}%", "%#{search}%", "%#{search.upcase}%").page(params[:page]||1).per(params[:per]||25)
+		page = params[:page] || 1
+		per = params[:per] || 25
+		@diseases = ::Dict::Disease.where("ICD10 LIKE ?  OR name LIKE ? OR py LIKE ?", "%#{search}%", "%#{search}%", "%#{search.upcase}%").page(page).per(per)
 		ret = @diseases.map { |e| e.to_ICD10_fhir_type }
 	    respond_to do |format|
 	      format.html # index.html.erb
-	      format.json { render json: {flag: true, info:"", data: ret} }
+	      format.json { render json: {flag: true, info:"success", data: ret} }
 	    end
 	end
 
-	# # GET
- #  # /dict/diseases/:id
-	# def show
-	#   respond_to do |format|
- #      format.html # show.html.erb
- #      format.json { render json: {flag: true, info:"", data: @disease} }
- #    end
-	# end
+	# 查询某一个病种信息  按ICD10模式显示 只返回json格式
+	# GET
+	def show
+		return render json:{flag: false, info:"病种id不能为空"} if params[:id].nil?
+		@disease = ::Dict::Disease.find(params[:id])
+		render json:{flag: true, info: "success", data: @disease.to_ICD10_fhir_type}
+	end
 
-	# # GET
-	# # GET /dict/diseases/new.json
-	# def new
-	#   @disease = ::Dict::Disease.new
-	#   respond_to do |format|
-	#     format.html # new.html.erb
-	#     format.json { render json: {flag: true, info:"", data: @disease} }
-	#   end
-	# end
-
-	# # GET
-	# # /dict/diseases/:id/edit
-	# def edit
-	# end
-
-	# 	# POST
-	# # /dict/diseases
-	# def create
-	#   p "==========create", params
-	#   @disease = ::Dict::Disease.new(params[:disease])
-	#   respond_to do |format|
-	#     if @disease.save
-	#       format.html { redirect_to @disease, notice: 'disease was successfully created.' }
-	#       format.json { render json: {flag: true, info:"", data: @disease} }
-	#     else
-	#       format.html { render action: "new" }
-	#       format.json { render json: @disease.errors, status: :unprocessable_entity }
-	#     end
-	#   end
-	# end
-
-	# 	# PUT
-	# # PUT /dict/diseases/:id
-	# def update
-	#   p "==========update", params
-	#   respond_to do |format|
-	#     if @disease.update_attributes(params[:disease])
-	#       format.html { redirect_to @disease, notice: 'disease was successfully updated.' }
-	#       format.json { render json: {flag: true, info:"", data: @disease} }
-	#     else
-	#       format.html { render action: "edit" }
-	#       format.json { render json: @disease.errors, status: :unprocessable_entity }
-	#     end
-	#   end
-	# end
-
-	# # DELETE
- #  # /dict/diseases/:id
-	# def destroy
-	# 	@disease.destroy
-	#   respond_to do |format|
-	#     format.html { redirect_to diseases_url }
-	#     format.json { head :no_content }
-	#   end
-	# end
-
-	# private
- #    # Use callbacks to share common setup or constraints between actions.
- #    def set_disease
- #      @disease = ::Dict::Disease.find(params[:id])
- #    end
+	def destroy
+		@disease = ::Dict::Disease.find(params[:id])
+	end
 end
