5eecded857fd7ffdc6491b26b696d7519c4c4e08
zyz <390024964@qq.com>
2018-06-23 23:19:40 +0800

    update_by_cmd

--- app/models/customer/fapiao.rb
+++ app/models/customer/fapiao.rb
@@ -1,2 +1,11 @@
 class Customer::Fapiao < ApplicationRecord
+	# default # 默认
+	# user_id
+	# name # 名称
+	# shuihao # 税号
+	# addr # 企业地址
+	# telephone # 电话号码
+	# bank # 开户行
+	# bank_num # 银行账号
+	# type_code # 企业1 / 个人2
 end
