75b3cd1edb0bdd90bd474d4fda010f7eabcce550
zengwei <zengwei@tenmind.com>
2018-06-23 18:57:56 +0800

    up

--- db/schema.rb
+++ db/schema.rb
@@ -9,5 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20180608080000) do
 end
