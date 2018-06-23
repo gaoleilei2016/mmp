c47b250f1fdc32aa945643ab42a2870812a4b3e6
zyz <390024964@qq.com>
2018-06-23 19:44:37 +0800

    update_by_cmd

--- db/migrate/20180612071351_create_ims_settings.rb
+++ db/migrate/20180612071351_create_ims_settings.rb
@@ -1,4 +1,4 @@
-class CreateImsSttrings < ActiveRecord::Migration[5.1]
+class CreateImsSettings < ActiveRecord::Migration[5.1]
   def change
     create_table :ims_settings do |t|
       t.string :org_ii,default:'',comment:'组织机构' 
@@ -6,7 +6,7 @@ class CreateImsSttrings < ActiveRecord::Migration[5.1]
       t.string :returned_at,default:'',comment:'可退药时间天数' 
       t.string :payment_Expired,default:'',comment:'付费未取药过期天数' 
       t.string :unpaid_expired,default:'',comment:'未支付订单过期天数' 
-      t.boolean :voice_prompts,default:'',comment:'是否语音提示' 
+      t.boolean :voice_prompts,comment:'是否语音提示' 
 
       t.timestamps
     end
