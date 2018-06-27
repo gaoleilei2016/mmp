cb6b7f10edc3183dd3a103cab3ecdaeec2cf1e14
[lgl] <2191269507@qq.com>
2018-06-27 18:04:37 +0800

    udpate

--- db/migrate/20180611105936_change_orders_orders.rb
+++ db/migrate/20180611105936_change_orders_orders.rb
@@ -2,6 +2,8 @@ class ChangeOrdersOrders < ActiveRecord::Migration[5.1]
   def change
     change_table :orders_orders do |t|
       t.integer :lock_version,comment:'乐观锁的锁版本号'
+      t.time :refund_medical_time,comment:'退药时间'
+      t.string :refund_medical_reason,default:'',comment:'退药原因'
     end
   end
 end
