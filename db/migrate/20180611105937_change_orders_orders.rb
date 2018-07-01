5df0f9869476117e7f8b8d73c8793a1dfe4ad341
qinxiao <qinxiao@tenmind.com>
2018-07-01 15:55:14 +0800

    up

--- db/migrate/20180611105937_change_orders_orders.rb
+++
@@ -1,7 +0,0 @@
-class ChangeOrdersOrders < ActiveRecord::Migration[5.1]
-  def change
-    change_table :orders_orders do |t|
-      t.integer :order_failure_time,comment:'订单失效时间'
-    end
-  end
-end
