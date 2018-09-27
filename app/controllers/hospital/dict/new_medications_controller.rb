05a3dc7018631dcacc27179c727da68ef95c8fb8
zengwei <zengwei@tenmind.com>
2018-09-27 15:55:41 +0800

    up

--- app/controllers/hospital/dict/new_medications_controller.rb
+++ app/controllers/hospital/dict/new_medications_controller.rb
@@ -12,21 +12,11 @@ class ::Hospital::Dict::NewMedicationsController < ApplicationController
   end
 
   # GET
-  # hospital/dict/medications
+  # hospital/dict/new_medications
   def index
     p "::Dict::MedicationsController index", params
+    serialno, ecode, name, common_name, alias_name, py, wb, common_py, common_wb, alias_py, alias_wb = Array.new(11, params[:search]) # 11是等号左边变量的数目
     p current_user,@cur_org
-    serialno = params[:search]
-    ecode = params[:search]
-    name = params[:search]
-    common_name = params[:search]
-    alias_name = params[:search]
-    py = params[:search]
-    wb = params[:search]
-    common_py = params[:search]
-    common_wb = params[:search]
-    alias_py = params[:search]
-    alias_wb = params[:search]
     search_str = "serialno LIKE ? OR ecode LIKE ? OR name LIKE ? OR common_name LIKE ? OR alias_name LIKE ? OR py LIKE ? OR wb LIKE ? OR common_py LIKE ? OR common_wb LIKE ? OR alias_py LIKE ? OR alias_wb LIKE ?"
     @medications = ::Hospital::Dict::NewMedication.where(hos_id: @cur_org.id).where(search_str, "%#{serialno}%", "%#{ecode}%", "%#{name}%", "%#{common_name}%", "%#{alias_name}%", "%#{py}%", "%#{wb}%", "%#{common_py}%", "%#{common_wb}%", "%#{alias_py}%", "%#{alias_wb}%").page(params[:page]||1).per(params[:per]||25)
     ret = @medications.map { |e| e.to_hash }
@@ -41,7 +31,7 @@ class ::Hospital::Dict::NewMedicationsController < ApplicationController
   end
 
   # GET
-  # hospital/dict/medications/:id
+  # hospital/dict/new_medications/:id
   def show
     @medication = ::Hospital::Dict::NewMedication.find(params[:id])
     # 获取该药品的实时库存量
@@ -51,9 +41,30 @@ class ::Hospital::Dict::NewMedicationsController < ApplicationController
 
   # 医生端 药品列表
   # GET
-  # hospital/dict/medications/list
+  # hospital/dict/new_medications/list
+  # {search: String, pharmacology_code: String, indications: Array}
   def list
-    
+    p "::Dict::MedicationsController index", params
+    serialno,ecode,name,common_name,alias_name,py,wb,common_py,common_wb,alias_py,alias_wb = Array.new(11, params[:search]) # 11是等号左边变量的数目
+    p current_user,@cur_org
+    search_str = "serialno LIKE ? OR ecode LIKE ? OR name LIKE ? OR common_name LIKE ? OR alias_name LIKE ? OR py LIKE ? OR wb LIKE ? OR common_py LIKE ? OR common_wb LIKE ? OR alias_py LIKE ? OR alias_wb LIKE ?"
+    # 基础查询 根据
+    @medications = ::Hospital::Dict::NewMedication.where(hos_id: @cur_org.id).where(search_str, "%#{serialno}%", "%#{ecode}%", "%#{name}%", "%#{common_name}%", "%#{alias_name}%", "%#{py}%", "%#{wb}%", "%#{common_py}%", "%#{common_wb}%", "%#{alias_py}%", "%#{alias_wb}%")
+    # 药理学分类查询
+    pharmacology_code = params[:pharmacology_code] # 药理学分类
+    @medications = @medications.where(pharmacology_code: pharmacology_code)
+    indications = params[:indications] # 适应症
+    @medications = @medications
+    @medications = @medications.page(params[:page]||1).per(params[:per]||25)
+    ret = @medications.map { |e| e.to_hash }
+    respond_to do |format|
+      if ret.blank?
+        format.json { render json: {flag: false, info: "没有相关药品", data: ret} }
+      else
+        format.html # index.html.erb
+        format.json { render json: {flag: true, info:"", data: ret} }
+      end
+    end
   end
 
   private
