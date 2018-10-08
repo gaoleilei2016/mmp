43635cb63cf843c00bfce94287cb2e49a0e2357c
weiqiongjin <1932341956@qq.com>
2018-10-08 16:46:06 +0800

    update_logs

--- app/models/ims/inv/refresh_log.rb
+++ app/models/ims/inv/refresh_log.rb
@@ -1,17 +1,17 @@
 class Ims::Inv::RefreshLog < ApplicationRecord
 
-	def self.get_log startDate=nil, endDate=nil
+	def self.get_log org_id,startDate=nil, endDate=nil
 		logs = nil
 		# startDate.present? && condition.merge!({:created_at.gt => Time.parse(startDate).beginning_of_day})
 		# endDate.present? && condition.merge!({:created_at.lt => Time.parse(endDate).end_of_day})
 		if(startDate.blank? && endDate.blank?)
-			logs = Ims::Inv::RefreshLog.all
+			logs = Ims::Inv::RefreshLog.where('org_id = ?',org_id)
 		elsif startDate.present? && endDate.present?
-			logs = Ims::Inv::RefreshLog.where("created_at > ? AND created_at < ?",Time.parse(startDate).beginning_of_day,Time.parse(endDate).end_of_day)
+			logs = Ims::Inv::RefreshLog.where("created_at > ? AND created_at < ? and org_id = ?",Time.parse(startDate).beginning_of_day,Time.parse(endDate).end_of_day,org_id)
 		elsif startDate.present?
-			logs = Ims::Inv::RefreshLog.where("created_at > ?",Time.parse(startDate).beginning_of_day)
+			logs = Ims::Inv::RefreshLog.where("created_at > ? and org_id = ? ",Time.parse(startDate).beginning_of_day,org_id)
 		else
-			logs = Ims::Inv::RefreshLog.where("created_at < ?",Time.parse(endDate).end_of_day)
+			logs = Ims::Inv::RefreshLog.where("created_at < ? and org_id = ？",Time.parse(endDate).end_of_day,org_id)
 		end
 		return logs
   	end
